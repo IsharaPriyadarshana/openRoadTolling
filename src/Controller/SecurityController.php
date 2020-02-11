706879938e692c63394534256d36d376ba7559e2
Ishara <isoftlive@gmail.com>
2020-02-11 14:02:25 +0530

    modular testing 1

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -29,6 +28,9 @@ class SecurityController extends AbstractController
      * @Route("/logout", name="app_logout")
      */
     public function logout(){
-        return new RedirectResponse('https://open-road-tolling.herokuapp.com/');
+        return $this->render('security/login.html.twig', [
+            'controller_name' => 'SecurityController'
+
+        ]);
     }
 }
