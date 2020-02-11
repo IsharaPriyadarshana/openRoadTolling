68053711d805c8f8c9d3262dc10f0d6b6b641e1f
Ishara <isoftlive@gmail.com>
2020-02-11 15:08:08 +0530

    modular testing 2

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -25,7 +25,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout", schemes={"https"})
+     * @Route("/logout", name="app_logout")
      */
 
     public function logout(){
